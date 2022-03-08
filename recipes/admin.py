95e3442bce1d1208576ddabe7979e15e23406df4
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-03-07 19:11:59 -0600

    Added models and registered them in admin page

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 
 
 # Register your models here.
+from recipes.models import Recipe, Measure, FoodItem, Ingredient, Step
+
+admin.site.register(Recipe)
+admin.site.register(Measure)
+admin.site.register(FoodItem)
+admin.site.register(Ingredient)
+admin.site.register(Step)
