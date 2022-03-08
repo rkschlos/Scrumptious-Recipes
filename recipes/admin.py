51766b1d59cb4482e22a91eee3fd1b18975c1924
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-03-07 21:38:59 -0600

    Added tag to models.py under the tag folder and updated admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 
 # Register your models here.
 from recipes.models import Recipe, Measure, FoodItem, Ingredient, Step
+from tags.models import Tag
 
 admin.site.register(Recipe)
 admin.site.register(Measure)
 admin.site.register(FoodItem)
 admin.site.register(Ingredient)
 admin.site.register(Step)
+admin.site.register(Tag)
