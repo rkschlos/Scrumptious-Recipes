d8454ea0c5d7972d63a513f586b6189289e9fbef
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-03-08 14:38:24 -0600

    downloaded markdownify and updated to requirememts.txt

--- scrumptious/settings.py
+++ scrumptious/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "recipes.apps.RecipesConfig",
+    "markdownify.apps.MarkdownifyConfig" "recipes.apps.RecipesConfig",
     "tags.apps.TagsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
